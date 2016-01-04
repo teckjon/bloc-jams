ff6282bb90f61ea31489129aca806d48d2c49f91
teckjon <teckjonlim@gmail.com>
2016-01-03 22:14:29 -0800

    assignment 22 completed: dom

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,29 +2,17 @@
  
      var points = document.getElementsByClassName('point');
  
-     var revealFirstPoint = function() {
-         points[0].style.opacity = 1;
-         points[0].style.transform = "scaleX(1) translateY(0)";
-         points[0].style.msTransform = "scaleX(1) translateY(0)";
-         points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+     var revealPoint = function(list) {
+         points[list].style.opacity = 1;
+         points[list].style.transform = "scaleX(1) translateY(0)";
+         points[list].style.msTransform = "scaleX(1) translateY(0)";
+         points[list].style.WebkitTransform = "scaleX(1) translateY(0)";
      };
- 
-     var revealSecondPoint = function() {
-         points[1].style.opacity = 1;
-         points[1].style.transform = "scaleX(1) translateY(0)";
-         points[1].style.msTransform = "scaleX(1) translateY(0)";
-         points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
- 
-     var revealThirdPoint = function() {
-         points[2].style.opacity = 1;
-         points[2].style.transform = "scaleX(1) translateY(0)";
-         points[2].style.msTransform = "scaleX(1) translateY(0)";
-         points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
- 
-     revealFirstPoint();
-     revealSecondPoint();
-     revealThirdPoint();
+    
+     for (var a = 0; a < points.length; a++) {
+
+     revealPoint(a);
+     }
+
  
  };
