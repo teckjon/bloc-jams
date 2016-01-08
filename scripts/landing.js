52874301d302b3ea60968fcbd9cac0b66407a0f8
teckjon <teckjonlim@gmail.com>
2016-01-08 00:56:15 -0800

    Assignment 23 completed, events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
- var animatePoints = function() {
+ var pointsArray = document.getElementsByClassName('point');
  
-     var points = document.getElementsByClassName('point');
+ var animatePoints = function(points) {
  
      var revealPoint = function(list) {
          points[list].style.opacity = 1;
@@ -8,11 +8,31 @@
          points[list].style.msTransform = "scaleX(1) translateY(0)";
          points[list].style.WebkitTransform = "scaleX(1) translateY(0)";
      };
-    
+     
      for (var a = 0; a < points.length; a++) {
-
-     revealPoint(a);
+        revealPoint(a);
      }
+ };
 
- 
+     var revealPoint = function(list) {
+         list.style.opacity = 1;
+         list.style.transform = "scaleX(1) translateY(0)";
+         list.style.msTransform = "scaleX(1) translateY(0)";
+         list.style.WebkitTransform = "scaleX(1) translateY(0)";
+     };
+     
+window.onload = function() {      
+     if (window.innerHeight > 950) {
+         forEach(pointsArray,revealPoint);
+     }
+         
+        var sellingPoints = document.getElementsByClassName('selling-points')[0];      
+        var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;         
+        
+         window.addEventListener('scroll', function(event) {
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             forEach(pointsArray,revealPoint);
+             //animatePoints(pointsArray);   
+         }
+     });   
  };
---
+++ scripts/utilities.js
@@ -0,0 +1,5 @@
+var forEach = function(set, callback) {
+    for (var i = 0; i < set.length; i++) {
+        callback(set[i]);
+    }
+} 
