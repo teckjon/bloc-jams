8d48a5b77761df16dfcb61c0f7eb8d2edcc68b84
teckjon <teckjonlim@gmail.com>
2016-01-08 16:25:28 -0800

    Checkpoint 23 Completed, Events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,32 +1,31 @@
  var pointsArray = document.getElementsByClassName('point');
- 
- var animatePoints = function(points) {
- 
-     var revealPoint = function(list) {
+
+ var animatePoints = function (points) {
+
+     var revealPoint = function (list) {
          points[list].style.opacity = 1;
          points[list].style.transform = "scaleX(1) translateY(0)";
          points[list].style.msTransform = "scaleX(1) translateY(0)";
          points[list].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
+     }
 
-     window.onload = function() {
-       
+     for (var a = 0; a < points.length; a++) {
+         revealPoint(a);
+     }
+ };
+
+ window.onload = function () {
+
      if (window.innerHeight > 950) {
          animatePoints(pointsArray);
      }
-         
-        var sellingPoints = document.getElementsByClassName('selling-points')[0];      
-        var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;         
-        window.addEventListener('scroll', function(event) {
+
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+     
+     window.addEventListener('scroll', function (event) {
          if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);   
+             animatePoints(pointsArray);
          }
      });
- }     
-     for (var a = 0; a < points.length; a++) {
-
-     revealPoint(a);
-     }
-
- 
  };
