43e2bf42baf26a6cefeac496efaf28155e1bfc75
teckjon <teckjonlim@gmail.com>
2016-01-10 21:10:19 -0800

    Assignment 25 in progress

--- scripts/album.js
+++ scripts/album.js
@@ -28,6 +28,22 @@
          { name: 'Wrong phone number', length: '2:15'}
      ]
  };
+
+ var albumTest = {
+     name: 'Test',
+     artist: 'Test Artist',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { name: 'Hello, Operator?', length: '1:01' },
+         { name: 'Ring, ring, ring', length: '5:01' },
+         { name: 'Fits in your pocket', length: '3:21'},
+         { name: 'Can you hear me now?', length: '3:14' },
+         { name: 'Wrong phone number', length: '2:15'}
+     ]
+ };
+
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
@@ -65,4 +81,11 @@
  
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+     
+     var albums = [albumPicasso, albumMarconi, albumTest];
+     var a = 0
+     albumImage.addEventListener("click", function(b) {
+         setCurrentAlbum(albums[a]);
+         a++;
+     });
  };
