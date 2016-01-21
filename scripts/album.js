0db4003120d75abe106aec8b4752e04a57f71888
teckjon <teckjonlim@gmail.com>
2016-01-20 22:16:57 -0800

    Checkpoint 33 completed, Library

--- scripts/album.js
+++ scripts/album.js
@@ -1,7 +1,24 @@
 var setSong = function(songNumber) {
+     if (currentSoundFile) {
+         currentSoundFile.stop();
+     }
+     
   currentlyPlayingSongNumber = parseInt(songNumber);
   currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
-};
+  currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+         // #2
+         formats: [ 'mp3' ],
+         preload: true
+  }); 
+ 
+     setVolume(currentVolume);
+ };
+ 
+ var setVolume = function(volume) {
+     if (currentSoundFile) {
+         currentSoundFile.setVolume(volume);
+     }
+};    
 
 var getSongNumberCell = function(number) {
     return $('.song-item-number[data-song-number="' + number + '"]')
@@ -27,13 +44,19 @@ var createSongRow = function(songNumber, songName, songLength) {
         if (currentlyPlayingSongNumber !== songNumber) {
             $(this).html(pauseButtonTemplate);
             setSong(songNumber);
+            currentSoundFile.play();
             updatePlayerBarSong();            
         } else if (currentlyPlayingSongNumber === songNumber) {
-            $(this).html(playButtonTemplate);
-            $('.main-controls .play-pause').html(playerBarPlayButton);            
-            setSong(songNumber); = null;
-            currentSongFromAlbum = null;
-        }        
+            if (currentSoundFile.isPaused()) {
+                $(this).html(pauseButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPauseButton);
+                currentSoundFile.play();
+            } else {
+                $(this).html(playButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPlayButton);
+                currentSoundFile.pause();
+            }
+        }
      };
       
      var onHover = function(event) {
@@ -112,6 +135,7 @@ var nextSong = function() {
     }
     
     setSong(currentSongList + 1);
+    currentSoundFile.play();
     currentSongFromAlbum = currentAlbum.songs[currentSongList];
     
     $('.currently-playing .song-name').text(currentSongFromAlbum.name);
@@ -141,6 +165,7 @@ var previousSong = function () {
     }
     
     setSong(currentSongList + 1);
+    currentSoundFile.play();
     currentSongFromAlbum = currentAlbum.songs[currentSongList];
     
     $('.currently-playing .song-name').text(currentSongFromAlbum.name);
@@ -165,6 +190,7 @@ var previousSong = function () {
  var currentlyPlayingSongNumber = null;
  var currentSongFromAlbum = null;
  var currentSoundFile = null; 
+ var currentVolume = 80;    
  var $previousButton = $('.main-controls .previous');
  var $nextButton = $('.main-controls .next');
 
